6a7bdefeeb93ff4a05ca4b51c5f0dbfb2c8b24be
wusi05 <wusi05@126.com>
2017-06-17 10:58:54 +0800

    Initial commit

---
+++ basic/Params.py
@@ -0,0 +1,40 @@
+# -*- coding: UTF-8 -*-
+# ** 代表字典
+params = {'job': 'wusi', 'name': 'very rich'}
+
+
+def story(**kwds):
+    return 'story 函数接受到的参数' '%(job)s called %(name)s.' % kwds
+
+
+print story(job='wusi', name='rich')
+# 注意这个地方添加**
+print story(**params)
+del params['name']
+print story(name='yuanyuan ', **params)
+
+
+def power(x, y, *others):
+    if others:
+        print '接受到的额外参数是:', others
+    return pow(x, y)
+
+
+print power(2, 3)
+print power(x=3, y=2)
+params = (5,) * 2
+print power(*params)
+print power(3, 3, 'hello world')
+
+
+def interval(start, stop=None, step=1):
+    '模拟 step大于0的序列'
+    if stop is None:
+        # 指定参数
+        start, stop = 0, start
+    result = []
+    i = start
+    while i < stop:
+        result.append(i)
+        i += step
+    return result
---
+++ basic/pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# pow() 方法返回 xy（x的y次方） 的值。
+
+import math
+
+print "math.pow(100, 2) : ", math.pow(100, 2)
+# 使用内置，查看输出结果区别, 内置的 pow() 方法
+# pow(x, y[, z]) 函数是计算x的y次方，如果z在存在，则再对结果进行取模，其结果等效于pow(x,y) %z
+print "pow(100, 2) : ", pow(100, 2)
+print "pow(100, 2, 99) : ", pow(100, 2, 99)
+
+print "math.pow(100, -2) : ", math.pow(100, -2)
+print "math.pow(2, 4) : ", math.pow(2, 4)
+print "math.pow(3, 0) : ", math.pow(3, 0)
