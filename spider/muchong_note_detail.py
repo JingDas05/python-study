d393d1b4bd14b2fbf6d11ca66f9f08ea44f3cd07
wusi05 <wusi05@126.com>
2018-05-05 18:42:51 +0800

    木虫爬虫帖子详情

--- spider/muchong_note_detail.py
+++ spider/muchong_note_detail.py
@@ -5,6 +5,9 @@
 
 from pyspider.libs.base_handler import *
 import hashlib
+import sys
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
 
 
 class Handler(BaseHandler):
@@ -30,27 +33,29 @@ class Handler(BaseHandler):
         note = {}
         # 获取整个doc
         context = response.doc
-        for each_note in context("tbody[id^='pid']"):
-            print(1)
-        first_note = context("tbody[id='pid3']")
-        author = first_note.find("div[class='pls_user'] h3 a")
-        author_link = author.attr("href")
-        # 楼层及创建时间块
-        floor_time_area = first_note.find("div[class='pls_info']")
-        create_time = floor_time_area("em").text()
-        # 一楼是1楼，二楼是沙发，三楼是板凳，四楼是4楼
-        floor = floor_time_area("span a").text()
-        raw_id = author.text() + create_time + floor
-        hl_md5.update(raw_id.encode(encoding='utf-8'))
-        note["id"] = hl_md5.hexdigest()
+        for each_note in context("tbody[id^='pid']").items():
+            author = each_note.find("div.pls_user h3 a")
+            author_link = author.attr("href")
+            # 楼层及创建时间块
+            floor_time_area = each_note.find("div[class='pls_info']")
+            create_time = floor_time_area("em").text()
+            # 一楼是1楼，二楼是沙发，三楼是板凳，四楼是4楼
+            raw_floor = floor_time_area("span a").text()
+            #floor = "2楼".encode("utf-8") if raw_floor=="沙发" else ("3楼" if raw_floor=="板凳" else raw_floor)
+            floor = "2楼"
+            print(type(floor))
+            raw_id = author.text() + create_time + floor[:-1]
+            print(raw_id)
+            #hl_md5.update(raw_id.encode(encoding='utf-8'))
+            #note["id"] = hl_md5.hexdigest()
+            #print(note)
         # 帖子内容区域
-        content_area = context.find("td[class='plc_mind'] div[class='plc_Con']")
+        #content_area = context.find("td[class='plc_mind'] div[class='plc_Con']")
         # 帖子内容?????
-        note["content"] = content_area("div[class='t_fsz'] td").text()
+        #note["content"] = content_area("div[class='t_fsz'] td").text()
         # 发布设备
-        note["device"] = content_area.find("font[class='gray_ext'] a")
+        #note["device"] = content_area.find("font[class='gray_ext'] a")
         # 楼主主题
-        note["title"] = content_area.find("h1").text()
+        #note["title"] = content_area.find("h1").text()
         # 回复的目标帖子id,包含了发帖人虫名， 发帖时间，以及楼层
-        note["reply_target_content_id"] = content_area.find("fieldset div").text()
-        print(note)
+        #note["reply_target_content_id"] = content_area.find("fieldset div").text()
